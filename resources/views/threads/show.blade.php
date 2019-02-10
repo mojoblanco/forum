cf3090b5b22baa772cff1d121777355a39b112ce
John Olawale <mojoblanco@gmail.com>
2019-02-10 22:26:24 +0100

    Thread can have replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,5 +13,24 @@
             </div>
         </div>
     </div>
+
+    
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @foreach ($thread->replies as $item)
+                <div class="card">
+                    <div class="card-header">
+                        <a href="#">{{ $item->owner->name }}</a>
+                        said {{ $item->created_at->diffForHumans() }}...
+                    </div>
+
+                    <div class="card-body">
+                        {{ $item->body }}
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 @endsection
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReadThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
+    /** @test **/
+    public function a_user_can_view_all_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test **/
+    public function a_user_can_view_a_single_thread()
+    {
+        $response = $this->get('/threads/' . $this->thread->id);
+
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test **/
+    public function a_user_can_read_replies_associated_to_a_thread ()
+    {
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get($this->thread->path())
+            ->assertSee($reply->body);
+    }
+}
